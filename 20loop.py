20af6307cba924d8b3755eb9a1ffc08d3601a5de
vaarunii <vakhanna@ucdavis.edu>
2023-06-28 16:02:56 -0700

    init

---
+++ 20loop.py
@@ -0,0 +1,18 @@
+# 20loop.py
+
+# Print out the numbers from 5 to less than 50 in steps of 7
+
+# Hint: use the 3 argument form of range()
+
+# Your code goes here
+
+"""
+python3 20loop.py
+5
+12
+19
+26
+33
+40
+47
+"""
